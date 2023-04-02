0bfdd9a6a2476ce1c69092b9f1755cf32b118a42
S <s@Mac-mini.local>
2023-04-02 20:36:57 +0900

    遷移アニメーション変更

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 
 <template>
   <div id="content">
-    <router-view></router-view>
-    <!-- <router-view v-slot="{ Component }">
-      <transition>
+    <!-- <router-view></router-view> -->
+    <router-view v-slot="{ Component }">
+      <transition mode="out-in">
         <component :is="Component" />
       </transition>
-    </router-view> -->
+    </router-view>
   </div>
 </template>
 
@@ -20,4 +20,18 @@
 .v-leave-to {
 	opacity: 0;
 } */
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(200px);
+}
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease-out;
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-200px);
+}
+
 </style>
