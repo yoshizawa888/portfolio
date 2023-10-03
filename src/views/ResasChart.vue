e394701f385f1e54bc973b30c6fd73df865d02b6
yoshizawa888 <81466163+yoshizawa888@users.noreply.github.com>
2023-10-03 22:34:26 +0900

    テキスト、画像変更

--- src/views/ResasChart.vue
+++ src/views/ResasChart.vue
@@ -1,19 +1,17 @@
 <script lang="ts" setup>
 import Detail from '../components/Detail.vue';
-import mainImg from '../assets/img/particle_wave.jpg';
+import mainImg from '../assets/img/resas_chart.jpg';
 </script>
 <template>
-	<Detail :imgUrl="mainImg" siteUrl="https://yoshizawa888.github.io/particle-wave/" gitUrl="https://github.com/yoshizawa888/particle-wave">
+	<Detail :imgUrl="mainImg" siteUrl="https://yoshizawa888.github.io/resas-chart/"
+		gitUrl="https://github.com/yoshizawa888/resas-chart">
 		<p>
-			こちらを作成しようと思ったきっかけは、学生の頃に卒業制作でUnityのVFXでパーティクルを触れたことがきっかけです。<br />
-			作っていて楽しくとても綺麗だったのでWebでもパーティクルがメインのものを作成してみました。
+			こちらは、RESAS APIで都道府県と人口を取得し、vue3-highchartsでグラフを出しています。
 		</p>
-		<br />
+		<br>
 		<p>
-			構造は比較的シンプルです。<br>
-			パーティクルが生成される2本の領域を生成し、左から右に流してサイン、コサイン、タンジェントで動きをつけています。<br>
-			色は赤を固定値にして青と緑は座標によって色が変わるようにしてグラデーションにしてみました。<br>
-			本当はもっとパーティクルを細かくして数を増やしたかったのですが、PCへの負荷を鑑みて今の形になっています。
+			制作した経緯はグラフを使ってみたかったことと、APIのデータ取得の練習です。<br>
+			さらに、コマンドでeslintが動くように簡単なコマンドも書いてみました。<br>
 		</p>
 	</Detail>
 </template>
